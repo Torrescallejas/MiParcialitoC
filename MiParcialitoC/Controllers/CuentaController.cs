fa7cbf13af8451ead5e2406f59e2a3f9a5c02a8b
Torrescallejas <ctorrescallejas@gmail.com>
2024-08-24 12:31:17 -0600

    Primer Commit

---
+++ MiParcialitoC/Controllers/CuentaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiParcialitoC.Controllers
+{
+    public class CuentaController : Controller
+    {
+        // Acción para mostrar la vista de Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // Acción para manejar el envío del formulario de Login (opcional)
+        [HttpPost]
+        public IActionResult Login(string email, string password, bool rememberMe)
+        {
+            // Aquí puedes manejar la autenticación del usuario
+            // y redirigir a la página correspondiente en caso de éxito
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
